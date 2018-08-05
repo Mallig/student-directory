28a869fa4eaf01edd09f669925649f8faab13bda
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-08-05 15:00:36 +0100

    puts student cohort

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
 students = [
-     "Dr. Hannibal Lecter",
-     "Darth Vader",
-     "Nurse Ratched",
-     "Michael Corleone",
-     "Alex Delarge",
-     "The Wicked Witch of the West",
-     "Terminator",
-     "Freddy Krueger",
-     "The Joker",
-     "Joffrey Baratheon",
-     "Norman Bates"
+     ["Dr. Hannibal Lecter", :november],
+     ["Darth Vader", :november],
+     ["Nurse Ratched", :november],
+     ["Michael Corleone", :november],
+     ["Alex Delarge", :november],
+     ["The Wicked Witch of the West", :november],
+     ["Terminator", :november],
+     ["Freddy Krueger", :november],
+     ["The Joker", :november],
+     ["Joffrey Baratheon", :november],
+     ["Norman Bates", :november]
     ]
 
 def print_header
@@ -17,14 +17,14 @@ def print_header
   puts "---------"
 end
 
-def print_names(names)
-  names.each { |name| puts name }
+def print(students)
+  students.each { |student| puts "#{student[0]} (#{student[1]} cohort)" }
 end
 
-def print_footer(names)
-  puts "Overall we have #{names.length} brilliant students."
+def print_footer(students)
+  puts "Overall we have #{students.length} brilliant students."
 end
 
 print_header
-print_names(students)
+print(students)
 print_footer(students)
